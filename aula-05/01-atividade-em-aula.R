438beec55f9a337366166a28b4744e26e845fefb
guilhermemirand <guilherme.mirand@gmail.com>
2018-05-21 21:25:46 -0300

    Aula 05

--- aula-05/01-atividade-em-aula.R
+++ aula-05/01-atividade-em-aula.R
@@ -1,15 +1,18 @@
 # Carregue a biblioteca tidyverse. Lembre que outras bibliotecas serão carregadas junto ao tidyverse
-
+library(tidyverse)
+library(tibble)
+library(lubridate)
 
 
 
 # Crie um dataframe com o conteúdo do arquivo ted_main.csv.gz. 
-
+ted_main <- read.csv("aula-05/data/ted_main.csv.gz")
 
 
 
 # Visualize o resumo dos dados do dataframe. Verifique os mínimos, máximos, médias e medianas das variáveis numéricas.
 # As variáveis duration, film_date e published_date estão no tipo de dados apropriado?
+summary(ted_main)
 
 
 
@@ -18,7 +21,10 @@
 #     * duration, para duração (em segundos). Experimente utilizar as funções as.duration e duration. Mantenha aquela que considerar mais apropriada.
 #     * film_date, para data, com a função as_datetime.
 #     * published_date, para data, com a função as_datetime..
-
+ted_main %>%
+  mutate(duration = duration(duration, units = "seconds"),
+         film_date = as_datetime(film_date), 
+         published_date = as_datetime(published_date)) -> ted_main_final
 
 
 
@@ -26,56 +32,90 @@
 #     * event
 #     * speaker_occupation
 
+ted_main_final %>% 
+  mutate(event = factor(event),
+         speaker_occupation = factor(speaker_occupation)) -> ted_main_final
+
 
 
 
 # Retire do dataframe a variável name
 
-
+ted_main_final %>% 
+  select(-name)
 
 
 # Visualize novamente o resumo dos dados do dataframe. Verifique os mínimos, máximos, médias e medianas das variáveis numéricas. Verifique as contagens das variáveis categóricas
 
-
+summary(ted_main_final)
 
 
 # Verifique quais registros possuem a menor quantidade de línguas. Corrija para que possuam no mínimo 1 idioma.
 
-
+ted_main_final %>% filter(languages > 0) -> ted_main_final
+  
+  
 
 
 # Verifique os 15 registros com menor data de filmagem. 
-
+ted_main_final %>%
+  arrange(film_date) %>%
+  head(15) %>%
+  View()
 
 
 
 # Crie um dataframe com a contagem de apresentações por ano de filmagem e visualize todo o seu conteúdo
+cont_apresentacoes <- 
+  ted_main_final %>% 
+  mutate(ano_filmagem = year(film_date)) %>%
+  group_by(ano_filmagem) %>%
+    summarise(qtd = n()) %>%
+  ungroup()
+  
+cont_apresentacoes %>% View()
 
 
 
 # Analise os 10 quantis da quantidade de apresentações por ano.
 # Descarte, do data frame de apresentações do TED Talks, aqueles cujo ano de filmagem tiver quantidade de apresentações menor ou igual à quantidade do quarto quantil.
 
+quarto_quantile <- quantile(cont_apresentacoes$ano_filmagem, probs = seq(0, 1, 0.1))[4]
 
+ted_main_final %>%
+  filter(year(film_date) > quarto_quantile) -> ted_main_final
+  
 
 
 # Verifique novamente o resumo dos dados do dataframe
-
+summary(ted_main_final)
 
 
 
 # Verifique os 10 registros com maior duração.
-
+ted_main_final %>%
+  arrange(desc(duration)) %>%
+  head(10) %>% 
+  View()
 
 
 
 # Existem apresentações com duração maior que 3 desvios padrão acima da média? Liste elas
-
+ted_main_final %>% 
+  mutate(corte = mean(duration) + (3 * sd(duration))) %>%
+  filter(as.numeric(duration) > corte) %>%
+  View()
 
 
 
 # Calcule os 4 quartis e o IQR da duração das apresentações. Liste as apresentações cuja duração supera 1.5 * o IQR + o terceiro quartil
 
+ted_main_final %>%
+  group_by(duration) %>%
+  summarise(qtd = n()) %>%
+  ungroup() -> duracao_apresent
+
+quantile(duracao_apresent$qtd, probs = seq(0, 1, 0.25))
 
 
 
